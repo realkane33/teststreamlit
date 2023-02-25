20e488749d160d46cadcda9cbba6a1c4afc83a5c
realkane33 <126335151+realkane33@users.noreply.github.com>
2023-02-26 00:14:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,6 @@ def to_excel(df):
     processed_data = output.getvalue()
     return processed_data
 df_xlsx = to_excel(releveur)
-st.write(releveur)
 st.download_button(label='📥 Download les  Resultats',
                                 data=df_xlsx ,
                                 file_name= 'df_test.xlsx')
