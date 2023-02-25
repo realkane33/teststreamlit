74897f6d024bfa27152e481a46f9769a42eda85e
realkane33 <126335151+realkane33@users.noreply.github.com>
2023-02-25 22:07:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from PIL import Image
 
 st.set_page_config(page_title='paiement releveurs')
 st.header('Paiement releveur')
-st.subheader("bestie bb lem je t'aime de srx")
+st.subheader("Fofa Todje")
 
 ### --- LOAD DATAFRAME
-a=st.file_uploader("charger le paiement des releveurs", type="xls")
-releveur=pd.read_excel(a)
+a=st.file_uploader("charger le paiement des releveurs")
+releveur=pd.read_excel(a.read())
 b=st.file_uploader("charger les compteurs", type="xlsx")
 compteurs=pd.read_excel(b) 
 forage=compteurs["forage"]
@@ -45,6 +45,7 @@ erreurs=pd.DataFrame()
 erreurs["sites"]=erreur
 releveur.to_excel("ncompteur.xlsx")
 erreurs.to_excel("sites mal orthographié.xlsx")
+st.download_button("telecharger",releveur)
 			
 		
 
