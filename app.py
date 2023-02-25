d782365ba574ba10a31d4df60eb62adce0eac604
realkane33 <126335151+realkane33@users.noreply.github.com>
2023-02-26 00:06:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 from PIL import Image
+from io import BytesIO
 
 st.set_page_config(page_title='paiement releveurs')
 st.header('Paiement releveur')
-st.subheader("Fofa Todje")
+st.subheader("test test")
 
 ### --- LOAD DATAFRAME
 a=st.file_uploader("charger le paiement des releveurs")
@@ -17,7 +18,7 @@ nombre=compteurs["nombre"]
 a=releveur["FORAGE"] 
 b=[]
 erreur=[]
-
+print(a)
 for i in range (len(a)):
 	c=a[i].split(",")
 	b.insert(i,c)
@@ -33,20 +34,32 @@ for objet in b:
 			k+=j.iloc[0,1]
          
 		except:
-			print(site.lstrip().lower()+" n existe pas sur la plateforme")
 			k="NA"
 			erreur.append(site.lstrip().lower())
 
 	n.insert(i,k)
 	 
 	i+=1
-releveur["nombreC"]=n
+releveur["Nombre de compteurs"]=n
 erreurs=pd.DataFrame()
 erreurs["sites"]=erreur
-releveur.to_excel("ncompteur.xlsx")
+c=releveur.to_excel("ncompteur.xlsx")
 erreurs.to_excel("sites mal orthographié.xlsx")
-st.download_button("telecharger",releveur)
-			
+
+
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output)
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+df_xlsx = to_excel(releveur)
+st.write(erreurs)
+st.write(releveur)
+st.download_button(label='📥 Download les  Resultats',
+                                data=df_xlsx ,
+                                file_name= 'df_test.xlsx')
 		
 
 
