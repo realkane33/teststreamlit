612dc67ad6ca428ce9dfe0e03683c75f92aadc4a
realkane33 <126335151+realkane33@users.noreply.github.com>
2023-02-25 20:25:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ bar_chart = px.bar(df_grouped,
 st.plotly_chart(bar_chart)
 
 # --- DISPLAY IMAGE & DATAFRAME
-col1, col2 = st.columns(2)
+col1, col2 = st.beta_columns(2)
 image = Image.open('survey.jpg')
 print(image)
 col1.image(image,
